49da13522ab64eb312b70f44818fe2ebadcfc18f
Mari Azh <Marzi.aghchay@gmail.com>
2023-02-28 11:35:50 +0000

    done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
+import React,{useState} from 'react';
 
 import './App.css';
 
 function App() {
+  const[count,setCount] =useState(0)
+  function contNumDe(){
+    setCount(c => c-1)
+  }
+  function contNumIn(){
+    setCount((i) => i + 1); 
+  }
+
   return (
     <div className='btn'>
-      <button>-</button>
-      <span> 0</span>
-      <button>+</button>
+      <button onClick={contNumDe}>-</button>
+      <span>{count}</span>
+      <button onClick={contNumIn}>+</button>
     </div>
   );
 }
