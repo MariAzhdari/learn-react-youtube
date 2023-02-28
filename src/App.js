74a01837d953c0323f4cc09c671cd0bbdefb0722
Mari Azh <Marzi.aghchay@gmail.com>
2023-02-28 19:43:12 +0000

    map

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import React,{useState} from 'react';
+import React from 'react';
+
 
 import './App.css';
 
-function App() {
-  const[count,setCount] =useState(0)
-  function contNumDe(){
-    setCount(c => c-1)
-  }
-  function contNumIn(){
-    setCount((i) => i + 1); 
-  }
 
-  return (
-    <div className='btn'>
-      <button onClick={contNumDe}>-</button>
-      <span>{count}</span>
-      <button onClick={contNumIn}>+</button>
-    </div>
+
+function App() {
+  // const[count,setCount] =useState(0)
+  // function contNumDe(){
+  //   setCount(c => c-1)
+  // }
+  // function contNumIn(){
+  //   setCount((i) => i + 1); 
+  // }
+
+  // return (
+  //   <div className='btn'>
+  //     <button onClick={contNumDe}>-</button>
+  //     <span>{count}</span>
+  //     <button onClick={contNumIn}>+</button>
+  //   </div>
+  // );
+
+  const Students = [
+    { name: "mari", isGirl: true },
+    { name: "mo", isGirl: false },
+    { name: "eli", isGirl: true },
+    { name: "Ali", isGirl: false },
+    { name: "maf", isGirl: false },
+    { name: "hani", isGirl: true },
+    { name: "sara", isGirl: true },
+    { name: "hadi", isGirl: false },
+  ];
+
+  return(
+<div>
+{Students.map(
+(S ,k ) => S.isGirl &&  <h1>{S.name}</h1>
+)}
+</div>
   );
 }
-
 export default App;
