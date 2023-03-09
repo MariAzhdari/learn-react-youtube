1580ae2da1443f91dac9a2d1fddc6a4a3c1c89f0
Mari Azh <Marzi.aghchay@gmail.com>
2023-03-09 15:01:30 +0000

    show and hide a text with use state

--- src/App.js
+++ src/App.js
@@ -2,17 +2,16 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
-  const[inputValue,setInputValue] = useState("");
-
-  const ClickInput = (event) => {
-   setInputValue(event.target.value);
-  };
-  return(
-    <div> 
-      <input type="text" onChange={ClickInput} />
-      {inputValue}
-    </div>
-  );
-
+  const[show,setShow] = useState(true);
+  
+  const showHandler=()=>{
+    setShow(!show)
+  }
+return (
+  <div style={{ textAlign: "center" }}>
+    <button onClick={showHandler}>show/hide</button>
+    { show ===true && <h1>Hi my name is Mari</h1>}
+  </div>
+);
 }
 export default App;
